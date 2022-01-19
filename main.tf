6de5008653a4f26ce5a91cefcea5c97bcacf9308
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-19 11:33:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,73 +3,6 @@ terraform {
   required_version = ">=0.12.13"
 }
 
-# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
-provider "aws" {
-  region  = "ap-northeast-1"
-ｈ
-}
-
-resource "aws_security_group" "terraform_test_sg" {
-  name        = "terraform_test"
-  description = "Allow http and https traffic."
-  vpc_id      = "vpc-f33c0a97" # デフォルトvpcのID
-  # ここにingressを書かず、ルールはaws_security_group_ruleを使って定義する
-}
-
-# 80番ポート許可のインバウンドルール
-resource "aws_security_group_rule" "inbound_http" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-
-  # ここでterraform_testセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.terraform_test_sg.id}"
-}
-
-# 443番ポート許可のインバウンドルール
-resource "aws_security_group_rule" "inbound_https" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-
-  # ここでterraform_testセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.terraform_test_sg.id}"
-}
-# 22番ポート許可のインバウンドルール
-resource "aws_security_group_rule" "inbound_ssh" {
-  type        = "ingress"
-  from_port   = 22
-  to_port     = 22
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-
-  # ここでterraform_testセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.terraform_test_sg.id}"
-}
-# 3389番ポート許可のインバウンドルール
-resource "aws_security_group_rule" "inbound_rdp" {
-  type        = "ingress"
-  from_port   = 3389
-  to_port     = 3389
-  protocol    = "tcp"
-  cidr_blocks = [
-    "0.0.0.0/0"
-  ]
-
-  # ここでterraform_testセキュリティグループに紐付け
-  security_group_id = "${aws_security_group.terraform_test_sg.id}"
-}
-
 resource "aws_instance" "ec2-jung-terraform-test1" {
   ami = "ami-063c5a5e375b71d95" #CentOS 8.4.2105 x86_64
   instance_type = "t2.micro"
@@ -78,5 +11,15 @@ resource "aws_instance" "ec2-jung-terraform-test1" {
   
   tags = {
     Name = "ec2-jung-terraform-test1"
+  }
+}
+resource "aws_instance" "ec2-jung-terraform-test2" {
+  ami = "ami-0902e32cadc848c1a" #Microsoft Windows Server 2019 Base
+  instance_type = "t2.micro"
+  key_name = "test"
+  vpc_security_group_ids = [aws_security_group.terraform_test_sg.id]
+
+  tags = {
+    Name = "ec2-jung-terraform-test2"
   }
 }

21c10da07ff91b4a8feda5b185756869501a84d7
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-01-19 11:33:04 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,8 @@
 ##----------------------------------------------------------
 ##Provider
 ##----------------------------------------------------------
-
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
   region ="ap-northeast-1"
+  version = "~> 2.36.0"
 }

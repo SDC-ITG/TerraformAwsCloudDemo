2f6e221c884f8f6693128d1a0c53071b9e278412
SDC-ITG <96460151+SDC-ITG@users.noreply.github.com>
2022-02-02 15:24:56 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+##----------------------------------------------------------
+##Resource
+##----------------------------------------------------------
+
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=1.1.0"
@@ -7,29 +11,20 @@ resource "aws_instance" "ec2-jung-terraform-test1" {
   ami = "ami-063c5a5e375b71d95" #CentOS 8.4.2105 x86_64
   instance_type = "t2.micro"
   key_name = "test"
-  vpc_security_group_ids = ["sg-0102f5ae6e3d2759a"]
+  vpc_security_group_ids = [aws_security_group.terraform_test_sg.id]
   
   tags = {
     Name = "ec2-jung-terraform-test1"
   }
 }
+
 resource "aws_instance" "ec2-jung-terraform-test2" {
   ami = "ami-0902e32cadc848c1a" #Microsoft Windows Server 2019 Base
   instance_type = "t2.micro"
   key_name = "test"
-  vpc_security_group_ids = ["sg-0102f5ae6e3d2759a"]
+  vpc_security_group_ids = [aws_security_group.terraform_test_sg.id]
 
   tags = {
     Name = "ec2-jung-terraform-test2"
-  }
-}
-resource "aws_instance" "ec2-jung-terraform-test3" {
-  ami = "ami-0902e32cadc848c1a" #Microsoft Windows Server 2019 Base
-  instance_type = "t2.micro"
-  key_name = "test"
-  vpc_security_group_ids =  ["sg-0102f5ae6e3d2759a"]
-
-  tags = {
-    Name = "ec2-jung-terraform-test3"
   }
 }
